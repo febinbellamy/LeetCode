fd7dff687aeaba3572bb2c6be09f7e1aaf9d6ed9
Febin Bellamy <febinbellamy@gmail.com>
2022-09-25 15:55:07 -0400

    747 - Largest Number At Least Twice Of Others

--- largestNumberAtleastTwiceOfOthers.js
+++ largestNumberAtleastTwiceOfOthers.js
@@ -3,16 +3,12 @@
  * @return {number}
  */
 var dominantIndex = function (nums) {
-  // if the largest element in the array --> Math.max(..nums) is atleast 2x larger than every other num in array
-  // return the index of the largest element. else, return -1;
 
-  // find the OG max value
   let maxNum = Math.max(...nums);
   let indexOfMaxNum = nums.indexOf(maxNum);
 
   nums.splice(indexOfMaxNum, 1);
 
-  // find new max value, see if its 2x less than the OG max. If so, return true. Else, false;
   let newMaxNum = Math.max(...nums);
 
   if (maxNum >= newMaxNum * 2) {
