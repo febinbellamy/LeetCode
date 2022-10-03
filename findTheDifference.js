e7569dd4044de6147eaf91e42d4dd260b2331393
Febin Bellamy <febinbellamy@gmail.com>
2022-10-03 00:07:04 -0400

    389 - Find the Difference

--- findTheDifference.js
+++ findTheDifference.js
@@ -17,3 +17,22 @@ var findTheDifference = function (s, t) {
   }
   return false;
 };
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function (s, t) {
+  if (!(0 <= s.length || s.length < 1000)) return false;
+  if (t.length !== s.length + 1) return false;
+
+  for (let i = 0; i < t.length; i++) {
+    let currentCharacterT = t[i];
+    let currentCharacterS = s[i];
+
+    if (currentCharacterT !== currentCharacterS) {
+      return currentCharacterT;
+    }
+  }
+  return false;
+};
