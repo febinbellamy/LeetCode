c3c98c2cc357be3fb99362b8d15ab0212aa17335
Febin Bellamy <febinbellamy@gmail.com>
2022-10-24 08:33:12 -0400

    367 - Valid Perfect Square

---
+++ validPerfectSquare.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function (num) {
+  for (let i = 0; i <= num; i++) {
+    if (i * i === num) {
+      return true;
+    }
+  }
+  return false;
+};
