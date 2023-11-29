c6c03c12e8290a183b4623c4f054e01cdaea5812
Febin Bellamy <febinbellamy@gmail.com>
2023-11-29 18:00:18 -0500

    Refactored Two Sum solution

--- twoSum.js
+++ twoSum.js
@@ -3,14 +3,21 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    
-    for (let i = 0; i < nums.length; i++) {
-        let currentNum = nums[i];
-        let num2 = target - currentNum;
-        if (nums.includes(num2) && nums.indexOf(num2) !== i) {
-            return [i, nums.indexOf(num2)];
-        } 
+var twoSum = function (nums, target) {
+  const visited = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let currentNum = nums[i];
+
+    if (!(currentNum in visited)) {
+      visited[currentNum] = i;
+      let difference = target - currentNum;
+      if (difference in visited && visited[difference] !== i) {
+        return [i, visited[difference]];
+      }
+    } else if (target === currentNum * 2) {
+      return [i, visited[currentNum]];
     }
-    return false;
+  }
+  return [];
 };
